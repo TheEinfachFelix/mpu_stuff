2d9c67cc2b665b50d2aac1fe32ae07678f7592a2
TheEinfachFelix <the.einfach.felix@gmail.com>
2023-01-21 18:08:19 +0100

    new data

--- V2/src/WIFIconfig.hpp
+++ V2/src/WIFIconfig.hpp
@@ -2,7 +2,7 @@
 
 #include <cinttypes>
 
-//const char* ssid = "PW5B";
-//const char* password = "51823423816189225268";
-const char* ssid = "pw:1234";
-const char* password = "14082005";
+const char* ssid = "PW5B";
+const char* password = "51823423816189225268";
+//const char* ssid = "pw:1234";
+//const char* password = "14082005";
