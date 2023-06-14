a9c2d5fef2f7b1c08955abc9a1ff2677a21efc36
TheEinfachFelix <the.einfach.felix@gmail.com>
2023-06-14 12:31:40 +0200

    added the function of the servo

--- V2/src/main.cpp
+++ V2/src/main.cpp
@@ -7,6 +7,8 @@ uint32_t StringLength = 9000; //6987 fine 6987 not fine 8000
 bool full = 0;
 double * AngleOut;
 int32_t altimeter_ofset;
+uint32_t Door_Time = 0;
+uint32_t Door_Delay = 1000;
 
 void setup() {
     Serial.begin(115200);
@@ -20,6 +22,8 @@ void setup() {
     bmp_setup();
     
     altimeter_ofset = get_bpm_SealevelPressure();
+
+    Landing_Gear.write(0);
 }
 
 void loop() {
@@ -30,10 +34,23 @@ void loop() {
     AngleOut = getAngle();
     if(x1.length() < StringLength){
         if(digitalRead(13) == 1 && !full){
+            if(Door_Time == 0){
+                Door_Time = int(millis());
+            }
+             if(millis() < (Door_Time + Door_Delay)){
+        Landing_Gear.write(0);
+    
+    } else {
+        Landing_Gear.write(180);  
+    }
             x1 = x1 + String((out.ax)) + "\t" + String((out.ay)) + "\t"+ String((out.az)) +"\t" + millis() 
             +"\t" + String(AngleOut[0]) +"\t" + String(AngleOut[1]) +"\t" + String(get_bpm_altitude(altimeter_ofset)) + "\n";
             Serial.println(x1.length());
-    }}
+    } else {
+        Door_Time = 0;
+        Landing_Gear.write(0);
+    }
+    }
 
     if(x1.length() >= StringLength){
         if(x2 == ""){
@@ -52,6 +69,8 @@ void loop() {
     //Serial.println(WiFi.localIP());
     //Serial.println(String((out.ax)) + "\t" + String((out.ay)) + "\t"+ String((out.az)) +"\t" + millis() +"\t" + String(AngleOut[0]) +"\t" + String(AngleOut[1]) + String(get_bpm_SealevelPressure()));
     delay(15);
-    Landing_Gear.write(0);
+    
 
+   
+    Serial.println(millis() > (Door_Time + Door_Delay));
 }
